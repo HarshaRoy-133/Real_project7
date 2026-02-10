b7925d367d1d7f23dddda81cc37f1e94507c2f05
Harsha Roy <sreeharsha2133@gmail.com>
2026-02-10 12:36:16 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,60 @@ import './App.css'
 
 export default function App() {
   const IMGURL = import.meta.env.BASE_URL;
-  const [isLoginView, setIsLoginView] = React.useState(true)
+  const [isLoginView, setIsLoginView] = React.useState(true);
 
   function switchView() {
-    setIsLoginView(isLoginView === true?false:true);
+    setIsLoginView(isLoginView===true?false:true);
   }
 
   return (
     <div className='app'>
       {isLoginView && (
         <div className='login-container'>
-          <h2>Sign in with email</h2>
+          <h2>Sign in with email </h2>
           <div className='input-group'>
-            <img className='left-icon' src={IMGURL + "mail.png"}alt=''/>
-            <input type='text' placeholder='Enter a emailID'/>
-            
+            <img className='left-icon'src={IMGURL+'mail.png'}alt='Email Icon'></img>
+            <input type='text' placeholder=' Enter a Email' />
             </div>
             <div className='input-group'>
-              <input type='{tooglePassword}' placeholder='Enter a valid password'/>
-              </div>
+              <img className='left-icon'src={IMGURL+'lock.png'}alt='lock Icon'></img>
+            <input type='password' placeholder=' Enter a Password' />
+            </div>
           <p>
             Don't have an account?{' '}
-            <label onClick={switchView}>Sign up</label>
+            <label onClick={switchView}>Sign up  </label>
+            <button type='button'>Sign in</button>
           </p>
+          <footer> M. Sree Harsha -- 2500031858---</footer>
         </div>
       )}
 
       {!isLoginView && (
         <div className='login-container'>
-          <h2>Register for a new account</h2>
+          <h2>Register for a account </h2>
+          <div className='input-group'>
+            <img className='left-icon'src={IMGURL+'pro.png'}alt='pro Icon'></img>
+            <input type='text' placeholder=' Enter a Full Name' />
+            </div>
+            <div className='input-group'>
+            <img className='left-icon'src={IMGURL+'pro.png'}alt='pro Icon'></img>
+            <input type='text' placeholder=' Enter a Last Name' />
+            </div>
+            <div className='input-group'>
+              <img className='left-icon'src={IMGURL+'phone.png'}alt='phone Icon'></img>
+            <input type='tel' placeholder=' Enter a Phone Number' />
+            </div>
+             <div className='input-group'>
+              <img className='left-icon'src={IMGURL+'lock.png'}alt='lock Icon'></img>
+            <input type='password' placeholder=' Enter a Password' />
+            </div>
+
+          <button type='button'>Sign up</button>
           <p>
             If you already have an account{' '}
-            <label onClick={switchView}>Sign in</label>
+            <label onClick={switchView}>Sign in </label>
           </p>
+          <footer> M. Sree Harsha -- 2500031858---</footer>
         </div>
       )}
     </div>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Real_project7/',
+  base:'/Real_project8/',
+
 })
